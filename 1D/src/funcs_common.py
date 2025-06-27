normalization fix

1D/src/funcs_common.py
@@ -7,14 +7,14 @@ class SimpleNN(nn.Module):
     def __init__(self, num_features, num_hidden):
         super(SimpleNN, self).__init__()
         self.hidden1 = nn.Linear(num_features, num_hidden)  # (inputs,hidden)
-        self.hidden2 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
-        self.hidden3 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
-        self.hidden4 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
+        # self.hidden2 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
+        # self.hidden3 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
+        # self.hidden4 = nn.Linear(num_hidden, num_hidden)  # (inputs,hidden)
 
         self.bn1 = nn.BatchNorm1d(num_features)
-        self.bn2 = nn.BatchNorm1d(num_hidden)
-        self.bn3 = nn.BatchNorm1d(num_hidden)
-        self.bn4 = nn.BatchNorm1d(num_hidden)
+        # self.bn2 = nn.BatchNorm1d(num_hidden)
+        # self.bn3 = nn.BatchNorm1d(num_hidden)
+        # self.bn4 = nn.BatchNorm1d(num_hidden)
         self.bn5 = nn.BatchNorm1d(num_hidden)
         self.output = nn.Linear(num_hidden, 1)  # (hidden,output)
 
@@ -25,8 +25,8 @@ class SimpleNN(nn.Module):
         # print("Flattened input shape:", x.shape)  # Debugging line
         x = self.bn1(x)
         x = torch.relu(self.hidden1(x))  # Activation hidden layer
-        x = self.bn2(x)
-        x = torch.relu(self.hidden2(x)) + x  # Activation hidden layer
+        # x = self.bn2(x)
+        # x = torch.relu(self.hidden2(x)) + x  # Activation hidden layer
         # x = self.bn3(x)
         # x = torch.relu(self.hidden3(x)) + x  # Activation hidden layer
         # x = self.bn4(x)
@@ -151,7 +151,7 @@ def timestepping(
         if IC_idx == 6:
             y = reeds_BC(y, num_x, N)
         y_prev = y
-
+    
     return y, sigf
 
 
@@ -194,18 +194,18 @@ def PN_update(
             yflux = yflux[:, :, None]
 
             y_NN, A_Dy_NN = preprocess_features(y_prev, A_Dy)
-            scaling = 1 / y_avg
-            inputs = scaling * torch.cat(
+            #scaling = 1 / y_avg
+            inputs = torch.cat(
                 (A_Dy_NN, sigt * y_NN, source_in, sigs * yflux), dim=-1
             )
             # print("Inputs shape:", inputs.shape)  # Debugging line
-            # exit(1)
+
             network_output = NN_model(inputs)
             sigf = network_output[:, :, 0]
-
+        #print(sigf.shape)
         if filter_type == 1:
             sigf0 = NN_model
-            sigf = sigf0 * torch.ones(batch_size, num_x)
+            sigf = sigf0*torch.ones(batch_size, num_x)
 
         if IC_idx == 6:
             sigf[:, 0] = sigf[:, 1]
@@ -252,7 +252,7 @@ def PN_update(
     y_update[:, :, 0] = (
         y_update[:, :, 0] + sigs[:, :, 0] * y_expand[:, 1 : num_x + 1, 0] + source
     )
-
+    #print('y update max = ', torch.max(y_update))
     return y_update, sigf
 
 

1D/src/params_common.py
@@ -2,64 +2,63 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-
-def filter_func(z, p):
-    return torch.exp(-(z**p))
-
+def filter_func(z,p):
+    return torch.exp(-z**p)
 
 # N = 3: 27.1199
 # N = 7: 16.1425
 # N = 9: 10.2298
 
-N = 9
+N       = 3
 N_exact = 127
-num_x = 128
-T = 0.5
+num_x   = 128
+T       = 0.5
 
-# filter type
-# 0 - Neural network
+# filter type 
+# 0 - Neural network 
 # 1 - Constant
 filter_type = 0
 
-xl = -1
-xr = 1
+xl      = -1
+xr      =  1
 
 
 filter_order = 4
 
-L = xr - xl
-dx = L / num_x
+L       = xr - xl
+dx      = L/num_x
 
-dt = dx / 2
-num_t = int((T + dt) // dt)
+dt      = dx/2
+num_t   = int((T+dt)//dt) 
 
-x_edges = torch.linspace(xl, xr, num_x + 1)
-x = torch.linspace(xl + dx / 2, xr - dx / 2, num_x)
+x_edges = torch.linspace(xl,xr,num_x + 1)
+x       = torch.linspace(xl+dx/2, xr- dx/2, num_x)
 
-num_features = 2 * N + 4
-num_hidden = N + 2
+num_features = 2*N+4
+num_hidden   = N+2
 
-filter = torch.zeros(N_exact + 1)
+filter = torch.zeros(N_exact+1)
 
-filt_input = torch.zeros(N + 1)
-filt_input[0 : N + 1] = torch.arange(0, N + 1, 1) / (N + 1)
-filter[0 : N + 1] = -torch.log(filter_func(filt_input, filter_order))
+filt_input        = torch.zeros(N+1)
+filt_input[0:N+1] = torch.arange(0,N+1,1)/(N+1)
+filter[0:N+1]     = -torch.log(filter_func(filt_input,filter_order))
 
-params = {
-    "num_x": num_x,
-    "num_t": num_t,
-    "N": N,
-    "N_exact": N_exact,
-    "num_features": num_features,
-    "num_hidden": num_hidden,
-    "filter": filter,
-    "dx": dx,
-    "dt": dt,
-    "x": x,
-    "xl": xl,
-    "xr": xr,
-    "x_edges": x_edges,
-    "L": L,
-    "T": T,
-    "filter_type": filter_type,
-}
+params = {'num_x'      : num_x,
+        'num_t'        : num_t,
+        'N'            : N,
+        'N_exact'      : N_exact,
+        'num_features' : num_features,
+        'num_hidden'   : num_hidden,
+        'filter'       : filter,
+        'dx'           : dx, 
+        'dt'           : dt,
+        'x'            : x,
+        'xl'           : xl,
+        'xr'           : xr,
+        'x_edges'      : x_edges,
+        'L'            : L,
+        'T'            : T,
+        'filter_type'  : filter_type}
+
+
+