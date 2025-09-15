ce5c8277a588c2a60a92dd16aa23b13e7ab3e471
benplumridge <134665632+benplumridge@users.noreply.github.com>
2025-09-15 15:37:44 -0400

    Add files via upload

--- 1D/scripts/train_all.py
+++ 1D/scripts/train_all.py
@@ -22,13 +22,13 @@ params["num_IC"] = 4
 params["batch_size"] = (
     20  ## make batch size a multiple of the number of Initial Conditions
 )
-params["num_epochs"] = 50
-params["learning_rate"] = 1e-1  # N = 3 lr = 1e-2,  N = 7 and 9 lr = 1e-3 
+params["num_epochs"] = 100
+params["learning_rate"] = 1e-2  # N = 3 lr = 1e-2,  N = 7 and 9 lr = 1e-3 
 #params["learning_rate"] = 1e2
 params["momentum_factor"] = 0.9
 params["sigs_max"] = 1
-
 GD_idx  = 1
+
 if GD_idx == 0:
     params["GD_optimizer"] = "SGD"
 elif GD_idx ==1:
@@ -43,6 +43,7 @@ for n in [3,7,9]:
     N = n
     params['N'] = N
     params['num_features'] = 2 * N + 4
+    params['num_hidden'] = N+2
     filt_input = torch.arange(0, N + 1, 1) / (N + 1)
     params['filter'] =  -torch.log(filter_func(filt_input, params['filter_order']))
     NN_model = training(params)    
